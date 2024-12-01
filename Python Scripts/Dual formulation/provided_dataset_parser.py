ab50aa8ec3d9f85e0520ac789d550d36f0ddc588
agl2000 <andreugimenezlarred@gmail.com>
2024-12-01 13:00:17 +0100

    last commit

--- Python Scripts/Dual formulation/provided_dataset_parser.py
+++ Python Scripts/Dual formulation/provided_dataset_parser.py
@@ -1,21 +1,8 @@
 import numpy as np
 import os
 
-
-
-#read all files in the relative '../../Data/provided_dataset' folder with os
-files = os.listdir('../../Data/provided_dataset')
-
-
-
-#filter files that contain the word 'train'
-train_files = [file for file in files if 'train' in file]
-
-for file in train_files:
-    with open(f'../../Data/provided_dataset/{file}', 'r') as f:
-        data = f.readlines()
-
-        # Initialize containers for X and y
+def parse_file(file, data=None):
+    # Initialize containers for X and y
     X = []
     y = []
 
@@ -34,7 +21,7 @@ for file in train_files:
     K = X.dot(X.T)
 
     # Write to AMPL data file format
-    with open(f'../../AMPL files/Dual formulation/parsed_{file}', 'w') as f:
+    with open(file, 'w') as f:
         print("X len",len(X))
         print("Y len",X.shape[1])
         f.write(f"param n := {len(X)};\n")
@@ -44,12 +31,40 @@ for file in train_files:
         for i, label in enumerate(y, start=1):
             f.write(f"{i} {label}\n")
         f.write(";\n")
-        f.write("param X :=\n")
+        colums=" ".join(map(str, range(1, X.shape[1] + 1)))
+        f.write("param X : "+colums+" :=\n")
         for i, point in enumerate(X, start=1):
-            f.write(f"[{i}, *] " + " ".join(map(str, point)) + "\n")
+            f.write(f"{i} " + " ".join(map(str, point)) + "\n")
+            
         f.write(";\n")
         f.write("param K :=\n")
         for i in range(len(X)):
             for j in range(len(X)):
                 f.write(f"[{i+1}, {j+1}] {K[i, j]}\n")
         f.write(";\n")
+
+
+#read all files in the relative '../../Data/provided_dataset' folder with os
+files = os.listdir('../../Data/provided_dataset')
+
+
+
+#filter files that contain the word 'train'
+train_files = [file for file in files if 'train' in file]
+
+for file in train_files:
+    with open(f'../../Data/provided_dataset/{file}', 'r') as f:
+        data = f.readlines()
+
+    parse_file(f'../../AMPL files/Dual formulation/parsed_{file}',data)
+
+
+# now parse the real dataset
+with open('../../Data/real_dataset/diabetes_train.dat', 'r') as f:
+    data = f.readlines()
+
+#substitute all the ',' for ' '
+data = [line.replace(',', ' ') for line in data]
+parse_file('../../AMPL files/Dual formulation/parsed_diabetes_train.dat',data)
+
+    
